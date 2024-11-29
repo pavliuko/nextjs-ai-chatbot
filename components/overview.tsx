10a6b622086b7d1fbda9ca42cdaa7956d0051c2e
pavliuko <pavliuk.aleksandr@gmail.com>
2024-11-29 18:42:06 +0000

    feat: Add PumaIcon component and update Overview to use it

--- components/overview.tsx
+++ components/overview.tsx
@@ -1,7 +1,7 @@
 import { motion } from 'framer-motion';
 import Link from 'next/link';
 
-import { MessageIcon, VercelIcon } from './icons';
+import { MessageIcon, VercelIcon, PumaIcon } from './icons';
 
 export const Overview = () => {
   return (
@@ -15,7 +15,7 @@ export const Overview = () => {
     >
       <div className="rounded-xl p-6 flex flex-col gap-8 leading-relaxed text-center max-w-xl">
         <p className="flex flex-row justify-center gap-4 items-center">
-          <VercelIcon size={32} />
+          <PumaIcon size={32} />
           <span>+</span>
           <MessageIcon size={32} />
         </p>
