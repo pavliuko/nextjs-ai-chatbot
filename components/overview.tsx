7e70a6bb33cd5da5099798d4aed3d9b6d601e7ed
pavliuko <pavliuk.aleksandr@gmail.com>
2024-11-30 09:42:08 +0000

    feat: Increase size of PumaIcon in Overview component for better visibility

--- components/overview.tsx
+++ components/overview.tsx
@@ -15,9 +15,7 @@ export const Overview = () => {
     >
       <div className="rounded-xl p-6 flex flex-col gap-8 leading-relaxed text-center max-w-xl">
         <p className="flex flex-row justify-center gap-4 items-center">
-          <PumaIcon size={32} />
-          <span>+</span>
-          <MessageIcon size={32} />
+          <PumaIcon size={64} />
         </p>
         <p>
           This is a Puma Browser research and development project. Have fun with it!
